046942032ba7a3c52e7a61eb2c0a06b852287cdb
babooteng2 <babooteng2@gmail.com>
2016-11-27 00:41:02 +0900

    JDBC와 H2 추가

--- hajiboot-layering/src/main/java/com/example/App.java
+++ hajiboot-layering/src/main/java/com/example/App.java
@@ -1,20 +1,24 @@
 package com.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.example.domain.Customer;
-import com.example.service.CustomerService;
 
 @EnableAutoConfiguration
-@ComponentScan
 public class App implements CommandLineRunner{
 	
 	@Autowired
-	CustomerService customerService;
+	NamedParameterJdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
@@ -22,10 +26,16 @@ public class App implements CommandLineRunner{
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		customerService.save(new Customer(1, "Nobita", "Nobi"));
-		customerService.save(new Customer(2, "Takeshi", "Goda"));
-		customerService.save(new Customer(3, "Suneo", "Honekawa"));
-		
-		customerService.findAll().forEach(System.out::println);
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		SqlParameterSource param = new MapSqlParameterSource()				
+				.addValue("id", 1);
+		Customer result = jdbcTemplate.queryForObject(sql, param,
+				new RowMapper<Customer>() {
+					@Override
+					public Customer mapRow(ResultSet rs, int rowNum) throws SQLException{
+						return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+					}
+				});
+		System.out.println("result : " + result);
 	}
 }
